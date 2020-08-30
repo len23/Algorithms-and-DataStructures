6bdb746089a81ac2e4901f5e9f31e5da76c3c7ab
len23 <lenin.montalvo.23@hotmail.com>
2020-08-30 13:03:12 -0500

    Solved the binary search

--- binary-search.js
+++ binary-search.js
@@ -1,7 +1,22 @@
-let arr = [2,5,8,9,10];
+let arrNumbers = [1, 3, 5, 7, 9];
 
 function binarySearch(list, item) { //Introduce the list and the item to search
-  const low =0;
-  let high = list.lenght - 1;
+  let low = 0; //Allways it'll be zero
+  let high = list.length - 1; //Calculate the last index
+  
+  while (low <= high) {
+    let mid = Math.floor((low + high)/2);
+    let guess = list[mid];
+    if(guess == item){
+      return mid;
+    }
+    if(guess > item) {
+      high = mid-1;
+    } else {
+      low = mid+1
+    }
+  }
+  return null;
+  
 }
-
+console.log(binarySearch(arrNumbers, -1));
